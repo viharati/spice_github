2509d3292ad46f719a40eb2f090f15d3021da0a6
kali <kali@kali>
2017-06-17 09:55:32 +0900

    linux test

--- MNA10_nonlinearCap.py
+++ MNA10_nonlinearCap.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+#:q-*- coding: utf-8 -*-
 ####
 import sys, os
 sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
@@ -7,3 +7,5 @@ import matplotlib.pyplot as plt
 
 dt=0.0001
 Vin=1
+
+
